dfed69217fc73a2720e7c781a580e37aa575d3c5
Kieran Llarena <baymax709@gmail.com>
2025-05-08 15:13:59 -0400

    init: src dir

--- server/src/index.js
+++ server/src/index.js
@@ -1,2 +1,37 @@
-console.log('Hello from index.js!');
+function appendTodoElementToList(todoLI, todoList) {
+  const now = new Date();
+  try {
+    todoList.appendChild(todoLI);
+    console.log(`${now} [LOG] Successfully appended todo item: ${todoLI}`);
+    return [true, null];
+  } catch (error) {
+    console.error(`${now} [ERROR] Failed to create todo item: ${error}`);
+    return [false, error];
+  }
+}
 
+function createTodoElement(todoContent) {
+  const now = new Date();
+  try {
+    const newTodoLI = document.createElement("li");
+    const newTodoBtn = document.createElement("button");
+
+    newTodoBtn.className = "hover:line-through cursor-pointer text-base";
+    newTodoBtn.innerText = todoContent;
+
+    newTodoLI.appendChild(newTodoBtn);
+
+    console.log(`${now} [LOG] Successfully created todo item: ${todoContent}`);
+
+    return [newTodoLI, null];
+  } catch (error) {
+    console.error(`${now} [ERROR] Failed to create todo item: ${error}`);
+    return [null, error];
+  }
+}
+
+function main() {
+  const myTodos = document.querySelector("#myTodos");
+  const addTodoBtn = document.querySelector("#addTodoBtn");
+}
+main();
